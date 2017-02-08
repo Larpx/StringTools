f75f302367deac583a04d6a0a6c5b073ba1f140f
Larpx <237198606@qq.com>
2017-02-08 17:36:56 +0800

    更新

---
+++ CommonLibrary/QRCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary
+{
+    public class QRCode
+    {
+
+    }
+}
